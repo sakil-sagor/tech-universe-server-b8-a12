e4f45fbbd907e8a8b0d1eb34bb503782019d9399
sakil-sagor <sakilsagor76543@gmail.com>
2023-11-24 14:34:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+// middlewares
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://assignment11-sakil.netlify.app"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+// const roomsrRoute = require("./routes/v1/rooms.route")
+// const accesstoken = require("./routes/v1/token.route");
+
+// app.use("/api/v1/rooms", roomsrRoute)
+// app.use("/api/v1/accesstoken", accesstoken)
+
+module.exports = app;
